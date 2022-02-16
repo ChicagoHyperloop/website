1fb5f37cc04d41b956936022bf2297cfa8e81744
unknown <zain@accucalc.com>
2022-02-15 18:59:54 -0600

    bingus

---
+++ JS/about.js
@@ -0,0 +1,22 @@
+import { loadExternalHTML } from "./scripts.js";
+
+/*
+     !!warning!! - you are about to use javascript, you may throw your computer out the window
+     JS for about.html
+     
+     TODO:
+          - loading external html
+          - implement skrollr for parallax
+
+*/
+
+function init() {
+     console.log("about.js loaded");
+}
+
+init();
+
+$(document).ready(function () {
+     loadExternalHTML();
+
+});
